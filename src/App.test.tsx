7ccd38ecabfca25d3d0665b22ee9bfd300f14aa6
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-16 20:13:10 +0200

    feat: setup UI and mock components (#4)
    * feat: add Material-UI
    
    * feat: add react-router-dom and initial dummy routes
    
    * feat: component restructing and Navigation component styling
    
    * feat: dummy Settings component and styling
    
    * feat: add and style the mock IssueList and IssueListItem components
    
    * feat: add and style the mock Search component
    
    - search bar based on 'material-ui-search-bar'
    
    * feat: add and style the mock Issue and IssueComment components

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders the App component", () => {
-  const { getByText } = render(<App />);
-  const headerElement = getByText(/MMS/i);
-  expect(headerElement).toBeInTheDocument();
-});
