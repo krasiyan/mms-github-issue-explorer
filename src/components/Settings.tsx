b56a8f2d62a62156f34826aa430b5d9b61476db2
Krasiyan Nedelchev <hello@krasiyan.com>
2020-10-17 02:32:01 +0200

    feat: settings persistence (#6)
    * feat: persist the settings values in the localStorage
    
    * feat: basic GitHub repository field validation in the Settings component
    
    * feat: basic GitHub token field validation in the Settings component
    
    * refactor: create a re-usable 'GithubSettingsTextField' component within Settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -12,6 +12,9 @@ import {
   FormControl,
 } from "@material-ui/core";
 
+import { defaultGitHubRepository, githubRepositoryRegex } from "../config";
+import { useStickyState } from "../helpers";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -31,74 +34,137 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export const Settings: React.FC<{}> = () => {
+interface GithubSettingsTextFieldProps {
+  fieldName: string;
+  fieldDefaultValue: string;
+  fieldRegex: RegExp;
+  formControlClassName: string;
+  id: string;
+  label: string;
+  placeholder: string;
+  helperText: React.ReactNode;
+  helperTextError?: React.ReactNode;
+  autoFocus: boolean;
+}
+
+const GithubSettingsTextField: React.FC<GithubSettingsTextFieldProps> = ({
+  fieldName,
+  fieldDefaultValue,
+  fieldRegex,
+  formControlClassName,
+  id,
+  label,
+  placeholder,
+  helperText,
+  helperTextError,
+  autoFocus,
+}) => {
   const classes = useStyles();
-  const [authorizationEnabled, setAuthorizationEnabled] = React.useState<
-    boolean
-  >(false);
-  const [githubRepository, setGithubRepository] = React.useState<string>("");
-  const [githubToken, setGithubToken] = React.useState<string>("");
+
+  const [value, setValue] = useStickyState<string>(
+    fieldName,
+    fieldDefaultValue
+  );
+  const isValid = (): boolean => fieldRegex.test(value);
+  const [isTouched, setTouched] = React.useState<boolean>(false);
 
   return (
-    <Grid
-      container
-      item={true}
-      md={10}
-      wrap="wrap"
-      justify="space-between"
-      alignItems="flex-end"
-      className={classes.root}
+    <FormControl
+      error={isTouched && !isValid()}
+      className={`${classes.field} ${
+        classes[formControlClassName as keyof typeof classes]
+      }`}
     >
-      <FormControl className={`${classes.field} ${classes.fieldRepository}`}>
-        <TextField
-          id="filled-full-width"
-          required={true}
-          value={githubRepository}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-            setGithubRepository(e.target.value)
-          }
+      <TextField
+        id={id}
+        required={true}
+        error={isTouched && !isValid()}
+        value={value}
+        onChange={({
+          target: { value: newValue },
+        }: React.ChangeEvent<HTMLInputElement>): void => setValue(newValue)}
+        autoFocus={autoFocus}
+        onFocus={!autoFocus ? (): void => setTouched(true) : undefined}
+        onBlur={autoFocus ? (): void => setTouched(true) : undefined}
+        label={label}
+        placeholder={placeholder}
+        helperText={
+          isTouched && !isValid() ? helperTextError || helperText : helperText
+        }
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{ spellCheck: "false" }}
+      />
+    </FormControl>
+  );
+};
+
+export const Settings: React.FC<{}> = () => {
+  const classes = useStyles();
+
+  const [authorizationEnabled, setAuthorizationEnabled] = useStickyState<
+    boolean
+  >("authorizationEnabled", false);
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <Grid
+        container
+        item={true}
+        md={10}
+        wrap="wrap"
+        justify="space-between"
+        alignItems="flex-end"
+      >
+        <GithubSettingsTextField
+          fieldName="githubRepository"
+          fieldDefaultValue={defaultGitHubRepository}
+          fieldRegex={githubRepositoryRegex}
+          formControlClassName="fieldRepository"
+          id="githubRepository"
           label="GitHub repository"
-          placeholder="https://github.com/facebook/react"
+          placeholder={defaultGitHubRepository}
           helperText={<span>Enter the full GitHub repository URL</span>}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </FormControl>
-      <FormControl className={classes.field}>
-        <FormLabel component="label">GitHub API authorization</FormLabel>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={authorizationEnabled}
-              name="authorizationEnabled"
-              onChange={({
-                target: { checked },
-              }: React.ChangeEvent<HTMLInputElement>): void =>
-                setAuthorizationEnabled(checked)
-              }
-            />
+          helperTextError={
+            <span>
+              Invalid GitHub repository - use the following format:
+              <br />
+              <strong>{defaultGitHubRepository}</strong>
+            </span>
           }
-          label="Authorized"
+          autoFocus={false}
         />
-        <FormHelperText>
-          Authorizing increases the request limits
-        </FormHelperText>
-      </FormControl>
-      {authorizationEnabled && (
-        <FormControl className={`${classes.field} ${classes.fieldToken}`}>
-          <TextField
-            id="filled-full-width"
-            label="GitHub personal access token"
-            required={true}
-            value={githubToken}
-            onChange={({
-              target: { value },
-            }: React.ChangeEvent<HTMLInputElement>): void =>
-              setGithubToken(value)
+
+        <FormControl className={classes.field}>
+          <FormLabel component="label">GitHub API authorization</FormLabel>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={authorizationEnabled}
+                name="authorizationEnabled"
+                onChange={({
+                  target: { checked },
+                }: React.ChangeEvent<HTMLInputElement>): void =>
+                  setAuthorizationEnabled(checked)
+                }
+              />
             }
-            focused={true}
-            autoFocus={true}
+            label="Authorized"
+          />
+          <FormHelperText>
+            Authorizing increases the request limits
+          </FormHelperText>
+        </FormControl>
+
+        {authorizationEnabled && (
+          <GithubSettingsTextField
+            fieldName="githubToken"
+            fieldDefaultValue={defaultGitHubRepository}
+            fieldRegex={new RegExp("(.+)")}
+            formControlClassName="fieldToken"
+            id="githubToken"
+            label="GitHub personal access token"
             placeholder="token"
             helperText={
               <span>
@@ -114,12 +180,10 @@ export const Settings: React.FC<{}> = () => {
                 </Link>
               </span>
             }
-            InputLabelProps={{
-              shrink: true,
-            }}
+            autoFocus={true}
           />
-        </FormControl>
-      )}
-    </Grid>
+        )}
+      </Grid>
+    </form>
   );
 };
